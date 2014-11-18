a1819b28b4925facb871a085979507b4f9c003bf
pengweiqiang <pengweiqiang@5782cfdc-cac2-4cb0-8122-00ee8e001a98>
2014-11-18 03:25:22 +0000

    用户接口api

--- bldj-android/src/com/bldj/lexiang/api/ApiUserUtils.java
+++ bldj-android/src/com/bldj/lexiang/api/ApiUserUtils.java
@@ -223,7 +223,6 @@ public class ApiUserUtils {
 			break;
 		}
 		
-		//TODO  要分几种返回情况
 		ApiUtils.getParseModel(params, ReqUrls.ADDRESS_MANAGER, false,
 				requestCallback, MethodType.UPDATE, context);
 	}
@@ -249,51 +248,51 @@ public class ApiUserUtils {
 	}
 	
 
-	/**
-	 * 获取用户个人中心界面
-	 * 
-	 * @param userId
-	 * @return
-	 */
-	public static void getUserInfo(Context context,
-			RequestCallback requestCallBack) {
-		Map<String, String> params = HttpClientAddHeaders.getHeaders(context);
-		// ApiUtils.getParseModel(params, ReqUrls.REQUEST_USER_INFO, false,
-		// requestCallBack,MethodType.USER_PAGE_INFO,null);
-	}
-
-	/**
-	 * 修改用户信息
-	 * 
-	 * @param enterId
-	 * @return
-	 */
-	public static void modifyUserInfo(Context context, String name,
-			String address, String mobile, RequestCallback requestCallBack) {
-		Map<String, String> params = HttpClientAddHeaders.getHeaders(context);
-		params.put(ReqUrls.NAME, name);
-		params.put(ReqUrls.MOBILE, mobile);
-		params.put(ReqUrls.ADDRESS, address);
-		ApiUtils.getParseModel(params, ReqUrls.UPDATE_USER_INFO, false,
-				requestCallBack, MethodType.GET_MAINPAGE_AD, null);
-	}
-
-	/**
-	 * 修改用户信息
-	 * 
-	 * @param
-	 * @return
-	 */
-	public static void downloadRepoInfo(Context context, long appId) {
-		Map<String, String> params = HttpClientAddHeaders.getHeaders(context);
-		params.put(ReqUrls.APP_ID, String.valueOf(appId));
-		ApiUtils.repo(params, ReqUrls.DOWNLOAD_REPO_INFO, HttpMethod.GET);
-		// ApiUtils.getParseModel(params, ReqUrls.DOWNLOAD_REPO_INFO, false,
-		// null,MethodType.DOWNLOAD_REPO_INFO,null);
-	}
-
-	// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	public static String parseLong2Str(long userId) {
-		return String.valueOf(userId);
-	}
+//	/**
+//	 * 获取用户个人中心界面
+//	 * 
+//	 * @param userId
+//	 * @return
+//	 */
+//	public static void getUserInfo(Context context,
+//			RequestCallback requestCallBack) {
+//		Map<String, String> params = HttpClientAddHeaders.getHeaders(context);
+//		// ApiUtils.getParseModel(params, ReqUrls.REQUEST_USER_INFO, false,
+//		// requestCallBack,MethodType.USER_PAGE_INFO,null);
+//	}
+//
+//	/**
+//	 * 修改用户信息
+//	 * 
+//	 * @param enterId
+//	 * @return
+//	 */
+//	public static void modifyUserInfo(Context context, String name,
+//			String address, String mobile, RequestCallback requestCallBack) {
+//		Map<String, String> params = HttpClientAddHeaders.getHeaders(context);
+//		params.put(ReqUrls.NAME, name);
+//		params.put(ReqUrls.MOBILE, mobile);
+//		params.put(ReqUrls.ADDRESS, address);
+//		ApiUtils.getParseModel(params, ReqUrls.UPDATE_USER_INFO, false,
+//				requestCallBack, MethodType.GET_MAINPAGE_AD, null);
+//	}
+//
+//	/**
+//	 * 修改用户信息
+//	 * 
+//	 * @param
+//	 * @return
+//	 */
+//	public static void downloadRepoInfo(Context context, long appId) {
+//		Map<String, String> params = HttpClientAddHeaders.getHeaders(context);
+//		params.put(ReqUrls.APP_ID, String.valueOf(appId));
+//		ApiUtils.repo(params, ReqUrls.DOWNLOAD_REPO_INFO, HttpMethod.GET);
+//		// ApiUtils.getParseModel(params, ReqUrls.DOWNLOAD_REPO_INFO, false,
+//		// null,MethodType.DOWNLOAD_REPO_INFO,null);
+//	}
+//
+//	// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//	public static String parseLong2Str(long userId) {
+//		return String.valueOf(userId);
+//	}
 }
