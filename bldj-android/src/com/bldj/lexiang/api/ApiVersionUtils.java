38d05567a5e91b65f41994158f92b1c7f46dbfff
pengweiqiang <pengweiqiang@5782cfdc-cac2-4cb0-8122-00ee8e001a98>
2014-11-18 03:25:31 +0000

    版本升级api

---
+++ bldj-android/src/com/bldj/lexiang/api/ApiVersionUtils.java
@@ -0,0 +1,31 @@
+package com.bldj.lexiang.api;
+
+import android.content.Context;
+
+import java.util.Map;
+
+import com.bldj.lexiang.GlobalConfig;
+import com.bldj.lexiang.constant.api.ReqUrls;
+import com.bldj.lexiang.constant.enums.MethodType;
+import com.bldj.lexiang.http.HttpClientAddHeaders;
+import com.bldj.lexiang.utils.HttpConnectionUtil.RequestCallback;
+/**
+ * 版本升级
+ * 
+ * @author zhuyb
+ * @email zhuyongb0@live.com
+ */
+public class ApiVersionUtils {		
+	/**
+	 * 获取最新版本信息
+	 * @param context
+	 * @param requestCallBack
+	 * @param adIndex
+	 */
+	public static void checkVersion(Context context,RequestCallback requestCallBack,int adIndex){
+		Map<String,String> params = HttpClientAddHeaders.getHeaders(context);
+        params.put("terType",params.get("source"));
+        params.put("vcode", GlobalConfig.VERSION_CODE_V);
+		ApiUtils.getParseModel(params, ReqUrls.CHECK_VERSION, false, requestCallBack,MethodType.GET_MAINPAGE_AD,null);
+	}
+}
