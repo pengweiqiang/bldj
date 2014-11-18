3049e20e217b4f4388165a7bf97224d5aa426311
pengweiqiang <pengweiqiang@5782cfdc-cac2-4cb0-8122-00ee8e001a98>
2014-11-18 03:23:38 +0000

    首页接口api

---
+++ bldj-android/src/com/bldj/lexiang/api/ApiHomeUtils.java
@@ -0,0 +1,30 @@
+package com.bldj.lexiang.api;
+
+import android.content.Context;
+
+import java.util.Map;
+
+import com.bldj.lexiang.constant.api.ReqUrls;
+import com.bldj.lexiang.constant.enums.MethodType;
+import com.bldj.lexiang.http.HttpClientAddHeaders;
+import com.bldj.lexiang.utils.HttpConnectionUtil.RequestCallback;
+/**
+ * 首页接口
+ * 
+ */
+public class ApiHomeUtils {		
+	/**
+	 * 获取首页广告位
+	 * @param context
+	 * @param limit
+	 * @param type
+	 * @param requestCallBack
+	 * @param adIndex
+	 */
+	public static void getAdList(Context context,int limit,int type,RequestCallback requestCallBack,int adIndex){
+		Map<String,String> params = HttpClientAddHeaders.getHeaders(context);
+        params.put(ReqUrls.LIMIT,String.valueOf(limit));
+        params.put("type",String.valueOf(type));
+		ApiUtils.getParseModel(params, ReqUrls.REQUEST_GET_MAINPAGE_AD, false, requestCallBack,MethodType.GET_MAINPAGE_AD,null);
+	}
+}
