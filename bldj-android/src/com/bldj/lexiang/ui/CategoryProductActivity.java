a86e501b09ec23309e95243ed2c3ff76a2db3b49
pengweiqiang <pengweiqiang@5782cfdc-cac2-4cb0-8122-00ee8e001a98>
2014-12-01 13:42:01 +0000

    分类界面

---
+++ bldj-android/src/com/bldj/lexiang/ui/CategoryProductActivity.java
@@ -0,0 +1,137 @@
+package com.bldj.lexiang.ui;
+
+import java.util.List;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ProgressBar;
+
+import com.bldj.gson.reflect.TypeToken;
+import com.bldj.lexiang.R;
+import com.bldj.lexiang.adapter.HomeAdapter;
+import com.bldj.lexiang.api.ApiProductUtils;
+import com.bldj.lexiang.api.vo.ParseModel;
+import com.bldj.lexiang.api.vo.Product;
+import com.bldj.lexiang.constant.api.ApiConstants;
+import com.bldj.lexiang.utils.DateUtils;
+import com.bldj.lexiang.utils.HttpConnectionUtil;
+import com.bldj.lexiang.utils.JsonUtils;
+import com.bldj.lexiang.utils.ToastUtils;
+import com.bldj.lexiang.view.ActionBar;
+import com.bldj.lexiang.view.XListView;
+import com.bldj.lexiang.view.XListView.IXListViewListener;
+
+/**
+ * 分类后的产品列表
+ * 
+ * @author will
+ * 
+ */
+public class CategoryProductActivity extends BaseActivity implements
+IXListViewListener {
+
+	ActionBar mActionBar;
+	private int categoryId;
+	private String categoryName;
+	
+	
+	private ProgressBar progressBar;
+	private XListView mListView;
+	private HomeAdapter listAdapter;
+	private List<Product> products;
+
+	private int pageNumber = 0;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		setContentView(R.layout.category_product);
+		super.onCreate(savedInstanceState);
+		categoryId = this.getIntent().getIntExtra("categoryId", 0);
+		categoryName = this.getIntent().getStringExtra("categoryName");
+		mActionBar = (ActionBar)findViewById(R.id.actionBar);
+		onConfigureActionBar(mActionBar);
+	}
+
+	// 设置activity的导航条
+	protected void onConfigureActionBar(ActionBar actionBar) {
+		actionBar.setTitle(categoryName);
+		actionBar.setLeftActionButton(R.drawable.ic_menu_back,
+				new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
+		actionBar.hideRightActionButton();
+	}
+
+	@Override
+	public void initView() {
+		progressBar = (ProgressBar) 
+				findViewById(R.id.progress_listView);
+		mListView = (XListView) findViewById(R.id.listview);
+	}
+
+	@Override
+	public void initListener() {
+		
+	}
+	
+	/**
+	 * 获取养生服务数据
+	 */
+	private void getData() {
+		ApiProductUtils.getProductByCategoryByid(CategoryProductActivity.this, pageNumber, ApiConstants.LIMIT,
+				categoryId,
+				new HttpConnectionUtil.RequestCallback() {
+
+					@Override
+					public void execute(ParseModel parseModel) {
+						progressBar.setVisibility(View.GONE);
+						mListView.setVisibility(View.VISIBLE);
+						if (!ApiConstants.RESULT_SUCCESS.equals(parseModel
+								.getStatus())) {
+							 ToastUtils.showToast(CategoryProductActivity.this,
+							 parseModel.getMsg());
+							 return;
+							
+						} else {
+							List<Product> productsList = JsonUtils.fromJson(
+									parseModel.getData().toString(),
+									new TypeToken<List<Product>>() {
+									});
+
+							if (pageNumber == 0) {
+								products.clear();
+							}
+							products.addAll(productsList);
+
+							listAdapter.notifyDataSetChanged();
+							onLoad();
+						}
+
+					}
+				});
+	}
+
+	@Override
+	public void onRefresh() {
+		pageNumber = 1;
+		getData();
+	}
+
+	@Override
+	public void onLoadMore() {
+		pageNumber++;
+		getData();
+	}
+
+	private void onLoad() {
+		mListView.stopRefresh();
+		mListView.stopLoadMore();
+		mListView.setRefreshTime(DateUtils.convert2String(
+				System.currentTimeMillis(), ""));
+	}
+
+}
--- bldj-android/src/com/bldj/lexiang/ui/HotProductFragment.java
+++ bldj-android/src/com/bldj/lexiang/ui/HotProductFragment.java
@@ -1,29 +1,54 @@
 package com.bldj.lexiang.ui;
 
-import com.bldj.lexiang.R;
-import com.bldj.lexiang.view.ActionBar;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import com.bldj.gson.reflect.TypeToken;
+import com.bldj.lexiang.R;
+import com.bldj.lexiang.adapter.CategoryAdapter;
+import com.bldj.lexiang.api.ApiProductUtils;
+import com.bldj.lexiang.api.vo.Category;
+import com.bldj.lexiang.api.vo.ParseModel;
+import com.bldj.lexiang.constant.api.ApiConstants;
+import com.bldj.lexiang.utils.DateUtils;
+import com.bldj.lexiang.utils.HttpConnectionUtil;
+import com.bldj.lexiang.utils.JsonUtils;
+import com.bldj.lexiang.utils.ToastUtils;
+import com.bldj.lexiang.view.ActionBar;
+import com.bldj.lexiang.view.XListView;
+import com.bldj.lexiang.view.XListView.IXListViewListener;
 /**
  * 热门推荐（分类）
  * @author will
  *
  */
-public class HotProductFragment extends BaseFragment{
+public class HotProductFragment extends BaseFragment implements IXListViewListener{
 	private View infoView;
 	private ActionBar mActionBar;
+	
+	private ProgressBar progressBar;
+	private XListView mListView;
+	private CategoryAdapter listAdapter;
+	private List<Category> categorys;
+	
+	private int pageNumber = 0;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
 		infoView = inflater.inflate(R.layout.hot_fragment, container, false);
-		mActionBar = (ActionBar) infoView.findViewById(R.id.actionBar);
-		onConfigureActionBar(mActionBar);
+		
+		
+		initView();
+
+		initListener();
 
 		return infoView;
 	}
@@ -34,4 +59,136 @@ public class HotProductFragment extends BaseFragment{
 		actionBar.hideLeftActionButton();
 		actionBar.hideRightActionButton();
 	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		categorys = new ArrayList<Category>();
+		listAdapter = new CategoryAdapter(mActivity, categorys);
+		mListView.setAdapter(listAdapter);
+		mListView.setPullLoadEnable(true);
+		mListView.setXListViewListener(this);
+		
+		getCategory();
+	}
+	/**
+	 * 初始化控件
+	 */
+	private void initView(){
+		mActionBar = (ActionBar) infoView.findViewById(R.id.actionBar);
+		onConfigureActionBar(mActionBar);
+		progressBar = (ProgressBar)infoView.findViewById(R.id.progress_listView);
+		mListView = (XListView)infoView.findViewById(R.id.listview);
+		
+		
+	}
+	/**
+	 * 事件初始化
+	 */
+	private void initListener(){
+		
+		
+	}
+	
+	/**
+	 * 获取收藏数据
+	 */
+	private void getCategory() {
+		ApiProductUtils.getCategory(mActivity, pageNumber, ApiConstants.LIMIT, new HttpConnectionUtil.RequestCallback() {
+
+			@Override
+			public void execute(ParseModel parseModel) {
+				progressBar.setVisibility(View.GONE);
+				mListView.setVisibility(View.VISIBLE);
+				if (!ApiConstants.RESULT_SUCCESS.equals(parseModel
+						.getStatus())) {
+					 ToastUtils.showToast(mActivity,
+					 parseModel.getMsg());
+					 return;
+//					List<Product> productsList = new ArrayList<Product>();
+//
+//					Product p1 = new Product();
+//					p1.setName("商品" + (products.size() + 1));
+//					p1.setPicurl("http://img02.taobaocdn.com/bao/uploaded/i3/T11iAAFoNbXXXXXXXX_!!0-item_pic.jpg_110x110.jpg");
+//
+//					Product p2 = new Product();
+//					p2.setName("商品" + (products.size() + 2));
+//					p2.setPicurl("http://img02.taobaocdn.com/bao/uploaded/i1/TB1aK_JGFXXXXXzXVXXXXXXXXXX_!!0-item_pic.jpg_110x110.jpg");
+//
+//					productsList.add(p1);
+//					productsList.add(p2);
+//					Product p3 = new Product();
+//					p3.setName("商品" + (products.size() + 3));
+//					p3.setPicurl("http://img.taobaocdn.com/bao/uploaded/TB10ChmGFXXXXadaXXXSutbFXXX.jpg");
+//					productsList.add(p3);
+//
+//					Product p4 = new Product();
+//					p4.setName("商品" + (products.size() + 4));
+//					p4.setPicurl("http://img02.taobaocdn.com/bao/uploaded/i1/TB1aK_JGFXXXXXzXVXXXXXXXXXX_!!0-item_pic.jpg_110x110.jpg");
+//					productsList.add(p4);
+//
+//					Product p5 = new Product();
+//					p5.setName("商品" + (products.size() + 5));
+//					p5.setPicurl("http://img.taobaocdn.com/bao/uploaded/TB1rpHzGpXXXXXJaXXXSutbFXXX.jpg");
+//					productsList.add(p5);
+//
+//					Product p6 = new Product();
+//					p6.setName("商品" + (products.size() + 6));
+//					p6.setPicurl("http://img.taobaocdn.com/bao/uploaded/TB1T2YnGpXXXXaFaXXXSutbFXXX.jpg");
+//					productsList.add(p6);
+//
+//					Product p7 = new Product();
+//					p7.setName("商品" + (products.size() + 7));
+//					p7.setPicurl("http://img01.taobaocdn.com/imgextra/i1/1713844438/TB2TXsCaXXXXXbuXXXXXXXXXXXX-1713844438.jpg");
+//					productsList.add(p7);
+//
+//					Product p8 = new Product();
+//					p8.setName("商品" + (products.size() + 8));
+//					p8.setPicurl("http://img.taobaocdn.com/bao/uploaded/TB1wguNGpXXXXcgXVXXSutbFXXX.jpg");
+//					productsList.add(p8);
+//					
+//					if(pageNumber==0){
+//						products.clear();
+//					}
+//					products.addAll(productsList);
+//
+//					listAdapter.notifyDataSetChanged();
+//					onLoad();
+
+				} else {
+					List<Category> categoryList = JsonUtils.fromJson(
+							parseModel.getData().toString(),
+							new TypeToken<List<Category>>() {
+							});
+					if(pageNumber==0){
+						categorys.clear();
+					}
+					categorys.addAll(categoryList);
+
+					listAdapter.notifyDataSetChanged();
+					onLoad();
+				}
+
+			}
+		});
+	}
+
+	@Override
+	public void onRefresh() {
+		pageNumber=0;
+		getCategory();
+	}
+
+	@Override
+	public void onLoadMore() {
+		pageNumber++;
+		getCategory();
+	}
+	private void onLoad() {
+		mListView.stopRefresh();
+		mListView.stopLoadMore();
+		mListView.setRefreshTime(DateUtils.convert2String(System.currentTimeMillis(),""));
+	}
+
+	
 }
